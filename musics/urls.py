d4129920ba88980fa4e866da56c6d072278086d5
yooseungju <seung338989@gmail.com>
2019-04-23 07:00:35 +0000

    03_comment edit && delete

--- musics/urls.py
+++ musics/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [ 
+    path('musics/<int:music_pk>/comments/<int:comment_pk>/', views.comment_update_and_delete),
     path('musics/<int:music_pk>/comments/', views.comment_create),
     path('artists/<int:artist_pk>/', views.artist_detail),
     path('artists/',views.artist_list),
--- musics/views.py
+++ musics/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import MusicSerializer, ArtistSerializer, ArtistDetailSerializer, CommentSerializer,MusicDetailSerializer
+from .serializers import MusicSerializer, ArtistSerializer, ArtistDetailSerializer, CommentSerializer, MusicDetailSerializer
 from .models import Music, Artist, Comment
 # Create your views here.
 # Response 를 통해 Serializer 를 반환
@@ -16,15 +16,14 @@ from .models import Music, Artist, Comment
 
 @api_view(['GET'])
 def music_list(request):
-    musics =  Music.objects.all()
+    musics = Music.objects.all()
     serializer = MusicSerializer(musics, many=True)
-        
     return Response(serializer.data)
     
-    
-def music_detail(request,music_pk):
+@api_view(['GET'])
+def music_detail(request, music_pk):
     music = get_object_or_404(Music, pk=music_pk)
-    serializer =MusicDetailSerializer(music)
+    serializer = MusicDetailSerializer(music)
     return Response(serializer.data)
     
 @api_view(['GET'])
@@ -32,18 +31,32 @@ def artist_list(request):
     artists = Artist.objects.all()
     serializer = ArtistSerializer(artists, many=True)
     return Response(serializer.data)
-    
+
 @api_view(['GET'])
 def artist_detail(request, artist_pk):
     artist = get_object_or_404(Artist, pk=artist_pk)
     serializer = ArtistDetailSerializer(artist)
     return Response(serializer.data)
-    
-    
+
 @api_view(['POST'])
 def comment_create(request, music_pk):
     serializer = CommentSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
         serializer.save(music_id=music_pk)
         return Response(serializer.data)
-    
+
+@api_view(['PUT', 'DELETE'])
+def comment_update_and_delete(request, music_pk, comment_pk):
+    comment = get_object_or_404(Comment, pk=comment_pk)
+    if request.method == 'PUT':
+        serializer = CommentSerializer(data=request.data, instance=comment)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({'message': 'Comment has been updated!!'})
+    else:
+        comment.delete()
+        return Response({'message': 'Comment has been deleted!!'})
+        
+        
+        
+        
